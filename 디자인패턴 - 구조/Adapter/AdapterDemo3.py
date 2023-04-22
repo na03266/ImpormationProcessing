062b40306ae46361a5a91f58e3d0f2eeb57b9c60
HwangjeNa <na03266@naver.com>
2023-04-22 11:51:36 +0900

    어댑터3- 1 계정정보 확인 테스트

--- 디자인패턴 - 구조/Adapter/AdapterDemo3.py
+++ 디자인패턴 - 구조/Adapter/AdapterDemo3.py
@@ -11,3 +11,10 @@ class KakaoAccount: #카카오 계정 살펴보기
         # 인증 절차 생략
         print("카카오 로그인 성공")
         return self.id + KakaoAccount.KAKAO_SECRET + self.password
+
+# 객체 생성 및 초기화
+account = KakaoAccount(id="my_id", password="my_password", name="John Doe", email="john@example.com")
+
+# 메서드 호출
+auth_token = account.get_auth_token()
+print(auth_token)
