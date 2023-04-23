462d3d0f2ac1c6c9ae6841584286007694c02ccc
HwangjeNa <na03266@naver.com>
2023-04-23 11:53:10 +0900

    어댑터3- 2 인프런 계정정보 확인

--- 디자인패턴 - 구조/Adapter/AdapterDemo3.py
+++ 디자인패턴 - 구조/Adapter/AdapterDemo3.py
@@ -18,3 +18,16 @@ account = KakaoAccount(id="my_id", password="my_password", name="John Doe", emai
 # 메서드 호출
 auth_token = account.get_auth_token()
 print(auth_token)
+
+class InflearnAccount: # 인프런 계정정보 살펴보기
+    INFLEARN_SECRET = "INF_SECRET"
+
+    def __init__(self, email, password, username):
+        self.email = email
+        self.password = password
+        self.username = username
+
+    def login(self):
+        # 인증 절차 생략
+        print("인프런 로그인 성공")
+        return self.email + InflearnAccount.INFLEARN_SECRET + self.password
