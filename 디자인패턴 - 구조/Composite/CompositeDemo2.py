fbd49afff31c127e127b534062bc4b10d013f5a2
HwangjeNa <na03266@naver.com>
2023-05-06 14:00:49 +0900

    전체 코드 중복 제거 및 정리

--- 디자인패턴 - 구조/Composite/CompositeDemo2.py
+++ 디자인패턴 - 구조/Composite/CompositeDemo2.py
@@ -1,12 +1,3 @@
-"""
-게임에서 상점 기능을 구현하려 한다.
-게임에 인벤토리가 존재하고 인벤토리에는 아이템을 넣을 수 있다.
-아이템 중에는 '가방' 이라는 아이템도 있다.
-가방은 인벤토리의 효율을 높여주기 위한 아이템으로 내부에 여러 개의 아이템을 더 보관할 수 있다.
-가방은 일종의 미니 인벤토리 기능을 하는 아이템이다.
-게임 내에서 '아이템 다 팔기' 기능을 사용하면, 가방 내부에 존재하는 아이템과 가방까지 전부 처분해주어야 한다.
-'인벤토리' 내부에 '가방' 이라는 것이 깊이 제한 없이 계속 중첩될 수 있는 트리 형태가 만들어진다.
-"""
 from abc import ABC, abstractmethod
 
 class Item(ABC):
@@ -28,93 +19,6 @@ class DefaultItem(Item):
 
     def get_name(self):
         return self.name
-
-
-class ItemStorage:
-    @abstractmethod
-    def add_item(self, item):
-        pass
-
-    @abstractmethod
-    def remove_item(self, item):
-        pass
-
-    @abstractmethod
-    def get_all_price(self):
-        pass
-
-class ItemStorage(ABC):
-    @abstractmethod
-    def add_item(self, item):
-        pass
-
-    @abstractmethod
-    def remove_item(self, item):
-        pass
-
-    @abstractmethod
-    def get_all_price(self):
-        pass
-
-class DefaultItemStorage(ItemStorage):
-    def __init__(self):
-        self.items = []
-
-    def add_item(self, item):
-        self.items.append(item)
-
-    def remove_item(self, item):
-        self.items.remove(item)
-
-    def get_all_price(self):
-        return sum(item.get_price() for item in self.items)
-
-class Inventory(DefaultItemStorage):
-    pass
-
-class Item:
-    def get_price(self):
-        pass
-
-    def get_name(self):
-        pass
-
-class DefaultItemStorage(ItemStorage):
-    def __init__(self):
-        self.items = []
-
-    def add_item(self, item):
-        self.items.append(item)
-
-    def remove_item(self, item):
-        self.items.remove(item)
-
-    def get_all_price(self):
-        return sum(item.get_price() for item in self.items)
-
-class ItemBag(DefaultItemStorage, Item):
-    def __init__(self, price, name):
-        super().__init__()
-        self.name = name
-        self.price = price
-
-    def get_price(self):
-        return self.get_all_price() + self.price
-
-    def get_name(self):
-        return self.name
-
-class Item:
-    def get_price(self):
-        pass
-
-class DefaultItem(Item):
-    def __init__(self, price, name):
-        self.price = price
-        self.name = name
-
-    def get_price(self):
-        return self.price
 
 class ItemStorage(ABC):
     @abstractmethod
