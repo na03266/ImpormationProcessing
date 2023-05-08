9afc36d24f37f5f00d859dcc1591a94740905ac4
HwangjeNa <na03266@naver.com>
2023-05-08 14:07:26 +0900

    패턴 설명 및 클래스 생성

---
+++ 디자인패턴 - 구조/Decorator/DecoratorDemo2.py
@@ -0,0 +1,19 @@
+"""
+데코레이션 패턴 설명
+래퍼 객체를 이용해 모듈과 비슷한 방식으로 기존 객체에 기능을 추가할 수 있다.
+런타임에 객체에 '행위' 혹은 '기능'을 추가할 수 있게 해준다.
+기존 객체를 '행위'를 가진 특별한 래퍼 객체 (데코레이터)에 넣어서 객체가 그 '행위'를 할 수 있게 만든다.
+캐싱, 로깅, 검증과 같은 기능에 쓰일 수 있다.
+"""
+
+class Pizza:
+    def pizza_name(self):
+        return "피자"
+
+class CheesePizza(Pizza):
+    def pizza_name(self):
+        return "치즈 " + super().pizza_name()
+
+class BulgogiPizza(Pizza):
+    def pizza_name(self):
+        return "불고기 " + super().pizza_name()
