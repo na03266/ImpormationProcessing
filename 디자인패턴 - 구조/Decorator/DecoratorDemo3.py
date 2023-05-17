a276415c223e6b00e686f6eab65952e9de1dbeee
HwangjeNa <na03266@naver.com>
2023-05-17 14:18:13 +0900

    중복 제거

--- 디자인패턴 - 구조/Decorator/DecoratorDemo3.py
+++ 디자인패턴 - 구조/Decorator/DecoratorDemo3.py
@@ -19,28 +19,6 @@ class Notifier:
     def send_email(self, email, message):
         print(f'이메일 주소: "{email}" 로 내용: "{message}" 을 보냈습니다.')
 
-# 예시 실행
-if __name__ == "__main__":
-    notifier = Notifier()
-    notifier.add_email("john@example.com")
-    notifier.add_email("jane@example.com")
-    notifier.send("안녕하세요! 파이썬으로 이메일을 보내는 예제입니다.")
-
-class Notifier:
-    def __init__(self):
-        self.emails = []
-
-    def add_email(self, email):
-        self.emails.append(email)
-        print(f'이메일 "{email}" 가 성공적으로 수신 이메일 목록에 추가되었습니다.')
-
-    def send(self, message):
-        for email in self.emails:
-            self.send_email(email, message)
-
-    def send_email(self, email, message):
-        print(f'이메일 주소: "{email}" 로 내용: "{message}" 을 보냈습니다.')
-
 class Client:
     def main(self):
         notifier = Notifier()
