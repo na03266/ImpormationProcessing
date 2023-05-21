ff45d28dd2cf6cda1d1c3b94a19fc74857ec8f0e
HwangjeNa <na03266@naver.com>
2023-05-21 14:25:55 +0900

    알람 클래스 코드 생성

--- 디자인패턴 - 구조/Decorator/DecoratorDemo3_1.py
+++ 디자인패턴 - 구조/Decorator/DecoratorDemo3_1.py
@@ -45,3 +45,72 @@ class SlackNotifierDecorator(NotifierBaseDecorator):
     def send(self, message: str):
         for email in self.notifier.get_emails():
             print(f"[슬랙 메세지 발신] \"{email}\" 로 내용: \"{message}\" 을 보냈습니다.")
+
+from typing import List, Type, Union
+from .notifier_interface import NotifierInterface
+from .notifier_base_decorator import NotifierBaseDecorator
+from .facebook_notifier_decorator import FacebookNotifierDecorator
+
+class Notifier(NotifierInterface):
+    def __init__(self):
+        self.notifiers: List[Union[NotifierBaseDecorator, NotifierInterface]] = []
+        self.emails: List[str] = []
+
+    def get_emails(self) -> List[str]:
+        return self.emails
+
+    def add_notifier(self, notifier: NotifierInterface):
+        self.notifiers.append(notifier)
+
+    def remove_notifier(self, notifier_class: Type[NotifierBaseDecorator]):
+        found_notifier = self.find_notifier(notifier_class)
+
+        if found_notifier is not None:
+            self.notifiers.remove(found_notifier)
+
+    def find_notifier(self, notifier_class: Type[NotifierBaseDecorator]) -> Union[NotifierBaseDecorator, None]:
+        for notifier in self.notifiers:
+            if isinstance(notifier, notifier_class):
+                return notifier
+
+        return None
+
+    def contain_notifier(self, notifier_class: Type[NotifierBaseDecorator]) -> bool:
+        return any(isinstance(notifier, notifier_class) for notifier in self.notifiers)
+
+    def decorate(self, decorator: Type[NotifierBaseDecorator]):
+        if self.contain_notifier(decorator):
+            print("이미 추가된 발신 타입입니다.")
+            return
+
+        try:
+            constructor = decorator.__init__
+            self.add_notifier(decorator(self))
+        except Exception as e:
+            print("예외 발생:", e)
+
+    def facebook_enabled(self, bool_value: bool):
+        if bool_value:
+            if self.contain_notifier(FacebookNotifierDecorator):
+                print("이미 활성화된 상태입니다.")
+                return
+
+            self.add_notifier(FacebookNotifierDecorator(self))
+        else:
+            self.remove_notifier(FacebookNotifierDecorator)
+
+    def add_email(self, email: str):
+        self.emails.append(email)
+        print(f'이메일 "{email}" 가 성공적으로 수신 이메일 목록에 추가되었습니다.')
+
+    def send(self, message: str):
+        if not self.notifiers:
+            print("등록된 NotifierInterface 가 없습니다.")
+            return
+
+        if not self.emails:
+            print("등록된 수신자가 없습니다.")
+            return
+
+        for notifier in self.notifiers:
+            notifier.send(message)
