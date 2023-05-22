cf8f5794605c2967c12688d2904336a39af74a0f
HwangjeNa <na03266@naver.com>
2023-05-22 14:27:49 +0900

    순환참조 수정, 확장 알람채널 지원, 타입힌트 추가

--- 디자인패턴 - 구조/Decorator/DecoratorDemo3_1.py
+++ 디자인패턴 - 구조/Decorator/DecoratorDemo3_1.py
@@ -5,13 +5,7 @@
 """
 
 from abc import ABC, abstractmethod
-
-class NotifierInterface(ABC):
-    @abstractmethod
-    def send(self, message):
-        pass
-
-from typing import Type
+from typing import List, Type, Union
 
 class NotifierInterface(ABC):
     @abstractmethod
@@ -19,37 +13,28 @@ class NotifierInterface(ABC):
         pass
 
 class NotifierBaseDecorator(NotifierInterface):
-    def __init__(self, notifier: Type[Notifier]):
+    def __init__(self, notifier: 'Notifier'):
         self.notifier = notifier
 
     @abstractmethod
     def send(self, message: str):
         pass
 
-from typing import List, Type
-from .notifier_base_decorator import NotifierBaseDecorator
-
 class EmailNotifierDecorator(NotifierBaseDecorator):
-    def __init__(self, notifier: Type[Notifier]):
+    def __init__(self, notifier: 'Notifier'):
         super().__init__(notifier)
 
     def send(self, message: str):
         for email in self.notifier.get_emails():
             print(f"[이메일 발신] \"{email}\" 로 내용: \"{message}\" 을 보냈습니다.")
 
-
 class SlackNotifierDecorator(NotifierBaseDecorator):
-    def __init__(self, notifier: Type[Notifier]):
+    def __init__(self, notifier: 'Notifier'):
         super().__init__(notifier)
 
     def send(self, message: str):
         for email in self.notifier.get_emails():
             print(f"[슬랙 메세지 발신] \"{email}\" 로 내용: \"{message}\" 을 보냈습니다.")
-
-from typing import List, Type, Union
-from .notifier_interface import NotifierInterface
-from .notifier_base_decorator import NotifierBaseDecorator
-from .facebook_notifier_decorator import FacebookNotifierDecorator
 
 class Notifier(NotifierInterface):
     def __init__(self):
