7edf39b49dc33e89b97b514bcc94841e64e7ead9
NaHwangje <na03266@naver.com>
2023-06-08 09:33:07 +0900

    각 업체에 대한 엘리베이터 클래스 정의

--- 디자인패턴 - 생성/AbstractFactory/AbstractFactoryDemo2.py
+++ 디자인패턴 - 생성/AbstractFactory/AbstractFactoryDemo2.py
@@ -15,3 +15,19 @@ class HyundaiElevatorFactory(ElevatorFactory):
 class SamsungElevatorFactory(ElevatorFactory):
     def create_elevator(self):
         return SamsungElevator()
+
+# Elevator 클래스들
+class LGElevator:
+    def __init__(self):
+        self.vendor = "LG"
+        # LG 특정 엘리베이터 초기화 코드 추가
+
+class HyundaiElevator:
+    def __init__(self):
+        self.vendor = "현대"
+        # 현대 특정 엘리베이터 초기화 코드 추가
+
+class SamsungElevator:
+    def __init__(self):
+        self.vendor = "삼성"
+        # 삼성 특정 엘리베이터 초기화 코드 추가
