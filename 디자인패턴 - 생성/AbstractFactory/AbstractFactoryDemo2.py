4768c9b44ea3609e34f06b10c81021e25bd8c12e
NaHwangje <na03266@naver.com>
2023-06-12 09:45:04 +0900

    클라이언트 메소드 변경, 싱글톤 적용방식 변경

--- 디자인패턴 - 생성/AbstractFactory/AbstractFactoryDemo2.py
+++ 디자인패턴 - 생성/AbstractFactory/AbstractFactoryDemo2.py
@@ -17,7 +17,6 @@ class LGElevatorFactory(ElevatorFactory):
         return cls._instance
 
     def _init_factory(self):
-        # 이곳에서 LG 팩토리의 초기화 작업을 수행합니다.
         pass
 
     def create_motor(self):
@@ -36,7 +35,6 @@ class HyundaiElevatorFactory(ElevatorFactory):
         return cls._instance
 
     def _init_factory(self):
-        # 이곳에서 현대 팩토리의 초기화 작업을 수행합니다.
         pass
 
     def create_motor(self):
@@ -55,7 +53,6 @@ class SamsungElevatorFactory(ElevatorFactory):
         return cls._instance
 
     def _init_factory(self):
-        # 이곳에서 삼성 팩토리의 초기화 작업을 수행합니다.
         pass
 
     def create_motor(self):
@@ -67,48 +64,48 @@ class SamsungElevatorFactory(ElevatorFactory):
 # Elevator 클래스들
 class LGElevator:
     def __init__(self):
-        self.vendor = "LG"
+        self.vendor = VendorID.LG
         # LG 특정 엘리베이터 초기화 코드 추가
 
 class HyundaiElevator:
     def __init__(self):
-        self.vendor = "현대"
+        self.vendor = VendorID.HYUNDAI
         # 현대 특정 엘리베이터 초기화 코드 추가
 
 class SamsungElevator:
     def __init__(self):
-        self.vendor = "삼성"
+        self.vendor = VendorID.SAMSUNG
         # 삼성 특정 엘리베이터 초기화 코드 추가
 
 # Elevator 부품 클래스들
 class LGMotor:
     def __init__(self):
-        self.vendor = "LG"
+        self.vendor = VendorID.LG
         # LG 모터 초기화 코드 추가
 
 class LGDoor:
     def __init__(self):
-        self.vendor = "LG"
+        self.vendor = VendorID.LG
         # LG 도어 초기화 코드 추가
 
 class HyundaiMotor:
     def __init__(self):
-        self.vendor = "현대"
+        self.vendor = VendorID.HYUNDAI
         # 현대 모터 초기화 코드 추가
 
 class HyundaiDoor:
     def __init__(self):
-        self.vendor = "현대"
+        self.vendor = VendorID.HYUNDAI
         # 현대 도어 초기화 코드 추가
 
 class SamsungMotor:
     def __init__(self):
-        self.vendor = "삼성"
+        self.vendor = VendorID.SAMSUNG
         # 삼성 모터 초기화 코드 추가
 
 class SamsungDoor:
     def __init__(self):
-        self.vendor = "삼성"
+        self.vendor = VendorID.SAMSUNG
         # 삼성 도어 초기화 코드 추가
 
 # Factory Method 패턴 구현
@@ -117,54 +114,37 @@ class ElevatorFactoryFactory:
     def get_factory(vendor_id):
         factory = None
 
-        if vendor_id == "LG":
+        if vendor_id == VendorID.LG:
             factory = LGElevatorFactory()
-        elif vendor_id == "현대":
+        elif vendor_id == VendorID.HYUNDAI:
             factory = HyundaiElevatorFactory()
-        elif vendor_id == "삼성":
+        elif vendor_id == VendorID.SAMSUNG:
             factory = SamsungElevatorFactory()
 
         return factory
 
+class VendorID:
+    LG = "LG"
+    HYUNDAI = "현대"
+    SAMSUNG = "삼성"
 
-vendor_id = "LG"  # 원하는 업체 ID로 변경하세요
-factory = ElevatorFactoryFactory.get_factory(vendor_id)
+def main():
+    vendor_name = input("업체 이름을 입력하세요 (LG, Samsung, Hyundai): ").lower()
 
-if factory:
-    elevator = factory.create_elevator()
-    print(f"{elevator.vendor} 엘리베이터를 생성했습니다.")
-else:
-    print("유효하지 않은 업체 ID입니다.")
+    if vendor_name not in [VendorID.LG, VendorID.SAMSUNG, VendorID.HYUNDAI]:
+        print("유효하지 않은 업체 이름입니다.")
+        return
 
-class Client:
-    @staticmethod
-    def main(vendor_name):
-        vendor_id = None
+    factory = ElevatorFactoryFactory.get_factory(vendor_name)
 
-        # 업체 이름에 따라 VendorID를 설정합니다.
-        if vendor_name.lower() == "lg":
-            vendor_id = "LG"
-        elif vendor_name.lower() == "samsung":
-            vendor_id = "삼성"
-        elif vendor_name.lower() == "hyundai":
-            vendor_id = "현대"
-        else:
-            print("유효하지 않은 업체 이름입니다.")
-            return
+    if factory:
+        motor = factory.create_motor()
+        door = factory.create_door()
 
-        factory = ElevatorFactoryFactory.get_factory(vendor_id)
-
-        # 생성된 팩토리를 사용하여 부품 생성 및 사용
-        if factory:
-            motor = factory.create_motor()
-            door = factory.create_door()
-
-            # 생성된 부품들을 사용하여 엘리베이터를 조립하거나 동작시키는 등의 작업 수행
-            print(f"{vendor_name} 엘리베이터 부품이 생성되었습니다.")
-        else:
-            print("유효하지 않은 업체 이름입니다.")
+        print(f"{vendor_name} 엘리베이터 부품이 생성되었습니다.")
+    else:
+        print("유효하지 않은 업체 이름입니다.")
 
 
 if __name__ == "__main__":
-    vendor_name = "LG"  # 테스트할 업체 이름을 변경해주세요.
-    Client.main(vendor_name)
+    main()
