3575a2284ac28dbda67feec0c870cf4c4e8a9138
NaHwangje <na03266@naver.com>
2023-06-18 10:37:47 +0900

    팩토리메소드 추가예제 구현

---
+++ 디자인패턴 - 생성/FactoryMethod/FactoryMethodDemo2.py
@@ -0,0 +1,41 @@
+# Abstract Factory 인터페이스
+class AbstractFactory:
+    def create_operation(self):
+        pass
+
+# ConcreteFactoryA 클래스
+class ConcreteFactoryA(AbstractFactory):
+    def create_operation(self):
+        return ConcreteProductA()
+
+# ConcreteFactoryB 클래스
+class ConcreteFactoryB(AbstractFactory):
+    def create_operation(self):
+        return ConcreteProductB()
+
+# IProduct 인터페이스
+class IProduct:
+    pass
+
+# ConcreteProductA 클래스
+class ConcreteProductA(IProduct):
+    def __init__(self):
+        print("ConcreteProductA 생성")
+
+# ConcreteProductB 클래스
+class ConcreteProductB(IProduct):
+    def __init__(self):
+        print("ConcreteProductB 생성")
+
+if __name__ == "__main__":
+    # 공장 객체 생성 (리스트)
+    factories = [
+        ConcreteFactoryA(),
+        ConcreteFactoryB()
+    ]
+
+    # 제품A 생성 (안에서 createOperation()와 생성 후처리 실행)
+    productA = factories[0].create_operation()
+
+    # 제품B 생성 (안에서 createOperation()와 생성 후처리 실행)
+    productB = factories[1].create_operation()
