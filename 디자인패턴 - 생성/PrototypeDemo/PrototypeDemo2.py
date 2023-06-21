d190655f127b0d73c52e4795bd43117f732e1761
NaHwangje <na03266@naver.com>
2023-06-21 11:45:35 +0900

    메뉴 GUI 구현

--- 디자인패턴 - 생성/PrototypeDemo/PrototypeDemo2.py
+++ 디자인패턴 - 생성/PrototypeDemo/PrototypeDemo2.py
@@ -12,3 +12,11 @@ class ToDoList:
             print("할 일 목록:")
             for idx, task in enumerate(self.tasks, start=1):
                 print(f"{idx}. {task}")
+
+def display_menu():
+    print("\n======= To-Do List =======")
+    print("1. 할 일 추가")
+    print("2. 할 일 목록 보기")
+    print("3. 종료")
+    choice = input("선택: ")
+    return choice
