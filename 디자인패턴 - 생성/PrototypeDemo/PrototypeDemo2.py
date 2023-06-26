c7fe20f04795ca08dbb595ddd7c42a038cbdf1a2
NaHwangje <na03266@naver.com>
2023-06-26 11:57:29 +0900

    텍스트 파일 생성 메소드 추가

--- 디자인패턴 - 생성/PrototypeDemo/PrototypeDemo2.py
+++ 디자인패턴 - 생성/PrototypeDemo/PrototypeDemo2.py
@@ -3,22 +3,24 @@ class ToDoList:
         self.tasks = []
 
     def add_task(self, task):
-        self.tasks.append(task)
+        self.tasks.append({"task": task, "completed": False})
 
     def show_tasks(self):
         if not self.tasks:
             print("할 일이 없습니다.")
         else:
             print("할 일 목록:")
-            for idx, task in enumerate(self.tasks, start=1):
-                print(f"{idx}. {task}")
-                
+            for idx, task_info in enumerate(self.tasks, start=1):
+                task_status = "완료" if task_info["completed"] else "미완료"
+                print(f"{idx}. [{task_status}] {task_info['task']}")
+
     def delete_task(self, task_idx):
         if 1 <= task_idx <= len(self.tasks):
             del self.tasks[task_idx - 1]
             print("할 일이 삭제되었습니다.")
         else:
             print("올바른 할 일 번호를 입력하세요.")
+
     def complete_task(self, task_idx):
         if 1 <= task_idx <= len(self.tasks):
             self.tasks[task_idx - 1]["completed"] = True
@@ -33,6 +35,14 @@ class ToDoList:
         else:
             print("올바른 할 일 번호를 입력하세요.")
 
+    def save_to_file(self):
+        with open("completed_tasks.txt", "w") as completed_file:
+            with open("incomplete_tasks.txt", "w") as incomplete_file:
+                for task_info in self.tasks:
+                    if task_info["completed"]:
+                        completed_file.write(task_info["task"] + "\n")
+                    else:
+                        incomplete_file.write(task_info["task"] + "\n")
 
 def display_menu():
     print("\n======= To-Do List =======")
@@ -41,7 +51,8 @@ def display_menu():
     print("3. 할 일 삭제")
     print("4. 할 일 완료 처리")
     print("5. 할 일 수정")
-    print("6. 종료")
+    print("6. 오늘 완료한 일들 저장")
+    print("7. 종료")
     choice = input("선택: ")
     return choice
 
@@ -68,6 +79,9 @@ def main():
             new_task = input("새로운 할 일 내용을 입력하세요: ")
             todo_list.modify_task(task_idx, new_task)
         elif choice == '6':
+            todo_list.save_to_file()
+            print("오늘 완료한 일들이 저장되었습니다.")
+        elif choice == '7':
             print("프로그램을 종료합니다.")
             break
         else:
