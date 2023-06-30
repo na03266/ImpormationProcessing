69f987c12a38aef312318556c630199e48d03f44
NaHwangje <na03266@naver.com>
2023-06-30 13:33:59 +0900

    최종 코드 수정, 메서드 별 인자 수정 및 생성방식 변경

--- 디자인패턴 - 생성/PrototypeDemo/PrototypeDemo2.py
+++ 디자인패턴 - 생성/PrototypeDemo/PrototypeDemo2.py
@@ -1,5 +1,8 @@
 import os
 from datetime import datetime
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QListWidget, QLineEdit, QPushButton, QLabel
+from PyQt5.QtCore import Qt
 
 class ToDoList:
     def __init__(self):
@@ -9,48 +12,25 @@ class ToDoList:
         self.tasks.append({"task": task, "completed": False})
 
     def show_tasks(self):
-        if not self.tasks:
-            print("할 일이 없습니다.")
-        else:
-            print("할 일 목록:")
-            for idx, task_info in enumerate(self.tasks, start=1):
-                task_status = "완료" if task_info["completed"] else "미완료"
-                print(f"{idx}. [{task_status}] {task_info['task']}")
+        return self.tasks
 
     def delete_task(self, task_idx):
-        if 1 <= task_idx <= len(self.tasks):
-            del self.tasks[task_idx - 1]
-            print("할 일이 삭제되었습니다.")
-        else:
-            print("올바른 할 일 번호를 입력하세요.")
+        if 0 <= task_idx < len(self.tasks):
+            del self.tasks[task_idx]
 
     def complete_task(self, task_idx):
-        if 1 <= task_idx <= len(self.tasks):
-            self.tasks[task_idx - 1]["completed"] = True
-            print("할 일이 완료 상태로 변경되었습니다.")
-        else:
-            print("올바른 할 일 번호를 입력하세요.")
+        if 0 <= task_idx < len(self.tasks):
+            self.tasks[task_idx]["completed"] = not self.tasks[task_idx]["completed"]
 
     def modify_task(self, task_idx, new_task):
-        if 1 <= task_idx <= len(self.tasks):
-            self.tasks[task_idx - 1]["task"] = new_task
-            print("할 일이 수정되었습니다.")
-        else:
-            print("올바른 할 일 번호를 입력하세요.")
+        if 0 <= task_idx < len(self.tasks):
+            self.tasks[task_idx]["task"] = new_task
 
-
-    def save_to_file(self):
-        today_date = datetime.now().strftime("%Y-%m-%d")
-        completed_filename = f"completed_tasks_{today_date}.txt"
-        incomplete_filename = f"incomplete_tasks_{today_date}.txt"
-
-        with open(completed_filename, "w") as completed_file:
-            with open(incomplete_filename, "w") as incomplete_file:
-                for task_info in self.tasks:
-                    if task_info["completed"]:
-                        completed_file.write(task_info["task"] + "\n")
-                    else:
-                        incomplete_file.write(task_info["task"] + "\n")
+    def save_to_file(self, filename):
+        with open(filename, "w") as file:
+            for task_info in self.tasks:
+                task_status = "completed" if task_info["completed"] else "incomplete"
+                file.write(f"{task_info['task']},{task_status}\n")
 
     def load_from_file(self, filename):
         if not os.path.isfile(filename):
@@ -58,78 +38,11 @@ class ToDoList:
 
         with open(filename, "r") as file:
             for line in file:
-                task = line.strip()
-                self.add_task(task)
-
-    def load_today_tasks(self):
-        today_date = datetime.now().strftime("%Y-%m-%d")
-        completed_filename = f"completed_tasks_{today_date}.txt"
-        incomplete_filename = f"incomplete_tasks_{today_date}.txt"
-
-        if os.path.isfile(completed_filename):
-            self.load_from_file(completed_filename)
-
-        if os.path.isfile(incomplete_filename):
-            self.load_from_file(incomplete_filename)
-
-def display_menu():
-    print("\n======= To-Do List =======")
-    print("1. 할 일 추가")
-    print("2. 할 일 목록 보기")
-    print("3. 할 일 삭제")
-    print("4. 할 일 완료 처리")
-    print("5. 할 일 수정")
-    print("6. 오늘 완료한 일들 저장")
-    print("7. 오늘 할 일 불러오기")
-    print("8. 종료")
-    choice = input("선택: ")
-    return choice
-
-def main():
-    todo_list = ToDoList()
-
-    while True:
-        choice = display_menu()
-
-        if choice == '1':
-            task = input("추가할 할 일을 입력하세요: ")
-            todo_list.add_task(task)
-            print("할 일이 추가되었습니다.")
-        elif choice == '2':
-            todo_list.show_tasks()
-        elif choice == '3':
-            task_idx = int(input("삭제할 할 일 번호를 입력하세요: "))
-            todo_list.delete_task(task_idx)
-        elif choice == '4':
-            task_idx = int(input("완료 처리할 할 일 번호를 입력하세요: "))
-            todo_list.complete_task(task_idx)
-        elif choice == '5':
-            task_idx = int(input("수정할 할 일 번호를 입력하세요: "))
-            new_task = input("새로운 할 일 내용을 입력하세요: ")
-            todo_list.modify_task(task_idx, new_task)
-        elif choice == '6':
-            todo_list.save_to_file()
-            print("오늘 완료한 일들이 저장되었습니다.")
-        elif choice == '7':
-            today_date = datetime.now().strftime("%Y-%m-%d")
-            completed_filename = f"completed_tasks_{today_date}.txt"
-            incomplete_filename = f"incomplete_tasks_{today_date}.txt"
-            todo_list = ToDoList()
-            todo_list.load_from_file(completed_filename)
-            todo_list.load_from_file(incomplete_filename)
-            print("오늘 할 일을 불러왔습니다.")
-        elif choice == '8':
-            print("프로그램을 종료합니다.")
-            break
-        else:
-            print("올바른 선택을 입력하세요.")
-
-if __name__ == "__main__":
-    main()
-
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QListWidget, QLineEdit, QPushButton, QLabel
-from datetime import datetime
+                task_data = line.strip().split(',')
+                if len(task_data) == 2:
+                    task = task_data[0]
+                    completed = True if task_data[1] == "completed" else False
+                    self.tasks.append({"task": task, "completed": completed})
 
 class ToDoListApp(QMainWindow):
     def __init__(self):
@@ -176,9 +89,11 @@ class ToDoListApp(QMainWindow):
 
         self.central_widget.setLayout(self.layout)
 
-        self.todo_list = []
+        self.todo_list = ToDoList()
+        self.load_from_file()
 
         self.update_today_date_label()
+        self.update_tasks_list()
 
     def update_today_date_label(self):
         today_date = datetime.now().strftime("%Y-%m-%d")
@@ -186,61 +101,41 @@ class ToDoListApp(QMainWindow):
 
     def add_task(self):
         task = self.add_task_input.text()
-        self.todo_list.append({"task": task, "completed": False})
-        self.add_task_input.clear()
-        self.update_tasks_list()
+        if task:
+            self.todo_list.add_task(task)
+            self.add_task_input.clear()
+            self.update_tasks_list()
 
     def delete_task(self):
-        selected_index = self.tasks_list_widget.currentRow()
-        if selected_index != -1:
-            del self.todo_list[selected_index]
+        selected_item = self.tasks_list_widget.currentItem()
+        if selected_item:
+            task_idx = self.tasks_list_widget.row(selected_item)
+            self.todo_list.delete_task(task_idx)
             self.update_tasks_list()
 
     def complete_task(self):
-        selected_index = self.tasks_list_widget.currentRow()
-        if selected_index != -1:
-            self.todo_list[selected_index]["completed"] = True
+        selected_item = self.tasks_list_widget.currentItem()
+        if selected_item:
+            task_idx = self.tasks_list_widget.row(selected_item)
+            self.todo_list.complete_task(task_idx)
             self.update_tasks_list()
 
     def update_tasks_list(self):
         self.tasks_list_widget.clear()
-        for task_info in self.todo_list:
+        tasks = self.todo_list.show_tasks()
+        for task_info in tasks:
             task_status = "Completed" if task_info["completed"] else "Incomplete"
             self.tasks_list_widget.addItem(f"{task_info['task']} ({task_status})")
 
     def save_to_file(self):
         today_date = datetime.now().strftime("%Y-%m-%d")
-        completed_filename = f"completed_tasks_{today_date}.txt"
-        incomplete_filename = f"incomplete_tasks_{today_date}.txt"
-
-        with open(completed_filename, "w") as completed_file:
-            with open(incomplete_filename, "w") as incomplete_file:
-                for task_info in self.todo_list:
-                    if task_info["completed"]:
-                        completed_file.write(task_info["task"] + "\n")
-                    else:
-                        incomplete_file.write(task_info["task"] + "\n")
+        filename = f"tasks_{today_date}.txt"
+        self.todo_list.save_to_file(filename)
 
     def load_from_file(self):
         today_date = datetime.now().strftime("%Y-%m-%d")
-        completed_filename = f"completed_tasks_{today_date}.txt"
-        incomplete_filename = f"incomplete_tasks_{today_date}.txt"
-
-        if not os.path.isfile(completed_filename) and not os.path.isfile(incomplete_filename):
-            return
-
-        self.todo_list = []
-
-        with open(completed_filename, "r") as completed_file:
-            for line in completed_file:
-                task = line.strip()
-                self.todo_list.append({"task": task, "completed": True})
-
-        with open(incomplete_filename, "r") as incomplete_file:
-            for line in incomplete_file:
-                task = line.strip()
-                self.todo_list.append({"task": task, "completed": False})
-
+        filename = f"tasks_{today_date}.txt"
+        self.todo_list.load_from_file(filename)
         self.update_tasks_list()
 
 if __name__ == '__main__':
