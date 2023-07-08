da341c023628c64406d1f23c35eccacd549082e5
NaHwangje <na03266@naver.com>
2023-07-08 14:22:28 +0900

    게임 기본 레이아웃 만들기

---
+++ minefinder/mine.py
@@ -0,0 +1,12 @@
+import pygame
+import sys
+
+# Pygame 초기화
+pygame.init()
+
+# 창 크기 설정
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# 창 타이틀 설정
+pygame.display.set_caption("지뢰찾기")
