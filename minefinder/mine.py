b1d56b784be9dbbfd88b6cb9464fb249db0b3c17
NaHwangje <na03266@naver.com>
2023-07-09 14:24:28 +0900

    그리드 생성 및 구현

--- minefinder/mine.py
+++ minefinder/mine.py
@@ -10,3 +10,29 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # 창 타이틀 설정
 pygame.display.set_caption("지뢰찾기")
+
+# 그리드 설정
+ROWS, COLS = 10, 10 # 10행 10열
+CELL_SIZE = 50 #50px
+
+# 색상 정의
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# 그리드 그리기
+def draw_grid():
+    for row in range(ROWS):
+        for col in range(COLS):
+            pygame.draw.rect(screen, WHITE, (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE), 1)
+
+# 게임 루프
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill(BLACK)
+    draw_grid()
+
+    pygame.display.update()
