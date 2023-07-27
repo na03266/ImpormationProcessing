86fa77e1afd1471429aa830c8c791eba9981c4e9
NaHwangje <na03266@naver.com>
2023-07-27 20:39:09 +0900

    생성 클래스들에 주석 추가

--- PrototypeDemo/animal.py
+++ PrototypeDemo/animal.py
@@ -9,23 +9,23 @@ class Animal:
         return copy.deepcopy(self)
 
 
-class Dog(Animal):#강아지 하위 클래스
+class Dog(Animal):#강아지 하위 클래스(동물클래스 상속)
     def __init__(self, name):
-        super().__init__('Dog')
+        super().__init__('Dog')  #상위 클래스 Animal 의 생성자 호출
         self.name = name
 
     def clone(self):
-        cloned_dog = super().clone()
-        cloned_dog.name = self.name + "_clone"
+        cloned_dog = super().clone()  #상위클래스 복제 메서드 호출
+        cloned_dog.name = self.name + "_clone" # 복제된 객체 이름 수정
         return cloned_dog
 
 
-class Cat(Animal):#고양이 하위 클래스
+class Cat(Animal):#고양이 하위 클래스 (동물 클래스 상속)
     def __init__(self, name):
-        super().__init__('Cat')
+        super().__init__('Cat') #상위 클래스 Animal 의 생성자 호출
         self.name = name
 
     def clone(self):
-        cloned_cat = super().clone()
-        cloned_cat.name = self.name + "_clone"
+        cloned_cat = super().clone() #상위클래스 복제 메서드 호출
+        cloned_cat.name = self.name + "_clone" # 복제된 객체 이름 수정
         return cloned_cat
--- PrototypeDemo/main.py
+++ PrototypeDemo/main.py
@@ -1,12 +1,13 @@
 from animal import Dog, Cat
 
 if __name__ == "__main__":
-    original_dog = Dog("Buddy")
+    original_dog = Dog("Buddy") # 원본 객체 생성
     original_cat = Cat("Whiskers")
 
-    cloned_dog = original_dog.clone()
-    cloned_cat = original_cat.clone()
+    cloned_dog = original_dog.clone() # 객체 복제
+    cloned_cat = original_cat.clone() 
 
+#원본과 복제된 동물 객체들의 정보 출력
     print(f"Original Dog: {original_dog.species}, Name: {original_dog.name}")
     print(f"Cloned Dog: {cloned_dog.species}, Name: {cloned_dog.name}")
 
