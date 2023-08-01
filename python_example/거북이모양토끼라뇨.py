1b7caf4d5f107c2b46bf136d1d493ba57edc90af
jijiyam <malody17120@naver.com>
2023-08-01 14:15:59 +0900

    방향 조절 기능 추가

---
+++ python_example/거북이모양토끼라뇨.py
@@ -0,0 +1,32 @@
+import turtle
+
+# Set up the turtle
+screen = turtle.Screen()
+screen.title("Control the Rabbit")
+screen.bgcolor("lightblue")
+
+rabbit = turtle.Turtle()
+rabbit.shape("turtle")
+rabbit.color("brown")
+rabbit.penup()
+
+# Function to move the rabbit forward
+def move_forward():
+    rabbit.forward(20)
+
+# Function to turn the rabbit left
+def turn_left():
+    rabbit.left(15)
+
+# Function to turn the rabbit right
+def turn_right():
+    rabbit.right(15)
+
+# Bind keyboard keys to the functions
+screen.listen()
+screen.onkeypress(move_forward, "Up")
+screen.onkeypress(turn_left, "Left")
+screen.onkeypress(turn_right, "Right")
+
+# Start the event loop
+turtle.done()
