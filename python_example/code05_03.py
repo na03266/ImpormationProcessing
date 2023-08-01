82bfaa094215fcdc56fdcf97655c09a50c438d3e
jijiyam <malody17120@naver.com>
2023-08-01 09:43:44 +0900

    사진띄우기

---
+++ python_example/code05_03.py
@@ -0,0 +1,9 @@
+from tkinter import *
+window = Tk()
+
+photo = PhotoImage(file="C:/Users/user/Desktop/source (2)/GIF/dog.gif")
+label1 = Label(window,image=photo)
+
+label1.pack()
+window.mainloop()
+
