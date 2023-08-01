a9053f9c40828d2a1be294b814ea9c33a4df2585
jijiyam <malody17120@naver.com>
2023-08-01 13:49:08 +0900

    배경호ㅏ면 바꿨는데 안바꿔짐;;;;;;;;;

--- python_example/챗gpt한테일시키기.py
+++ python_example/챗gpt한테일시키기.py
@@ -1,21 +1,43 @@
-def read_text_file(file_path):
-    try:
+import tkinter as tk
+from tkinter import filedialog
+
+def open_file():
+    file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
+    if file_path:
         with open(file_path, 'r') as file:
             content = file.read()
-            return content
-    except FileNotFoundError:
-        print(f"Error: File '{file_path}' not found.")
-        return None
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
+            text.delete(1.0, tk.END)
+            text.insert(tk.END, content)
 
-def main():
-    file_path = input("Enter the path to the text file: ") #텍스트 파일 경로 넣는 곳 
-    content = read_text_file(file_path)
-    if content is not None:
-        print("File content:")
-        print(content)
+def close_file():
+    text.delete(1.0, tk.END)
 
-if __name__ == "__main__":
-    main()
+# Create the main application window
+root = tk.Tk()
+root.title("Text File Viewer")
+
+# Load the background image
+bg_image = tk.PhotoImage(file="C:/Users/user/Desktop/source (2)/GIF/dog13.gif")
+
+# Create a label to hold the background image
+background_label = tk.Label(root, image=bg_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+# Create a menu bar
+menubar = tk.Menu(root)
+root.config(menu=menubar)
+
+# Create a "File" menu
+file_menu = tk.Menu(menubar, tearoff=0)
+menubar.add_cascade(label="File", menu=file_menu)
+
+# Add menu items for "Open" and "Close"
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Close", command=close_file)
+
+# Create a text widget to display file content
+text = tk.Text(root, wrap=tk.WORD)
+text.pack(expand=True, fill='both')
+
+# Run the main event loop
+root.mainloop()
